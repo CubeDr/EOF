12317e706b7fd9d199a8f16211fdf912a91132aa
CubeDr <spaceship00@naver.com>
2017-09-06 12:13:07 +0900

    액티비티 전환

--- app/src/main/java/kr/ac/hanyang/eos/eof/MainActivity.java
+++ app/src/main/java/kr/ac/hanyang/eos/eof/MainActivity.java
@@ -1,7 +1,11 @@
 package kr.ac.hanyang.eos.eof;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import kr.ac.hanyang.eos.eof.game.GameActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/kr/ac/hanyang/eos/eof/game/GameActivity.java
@@ -0,0 +1,15 @@
+package kr.ac.hanyang.eos.eof.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.ac.hanyang.eos.eof.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
