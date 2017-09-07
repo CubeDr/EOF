1ee012153381c69212a23f717af98775086f5277
CubeDr <spaceship00@naver.com>
2017-09-07 09:04:17 +0900

    Level skeleton

---
+++ app/src/main/java/kr/ac/hanyang/eos/eof/game/Level.java
@@ -0,0 +1,25 @@
+package kr.ac.hanyang.eos.eof.game;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by space on 2017-09-07.
+ */
+
+public abstract class Level {
+    public int level;
+    public String[] developers;
+
+
+    public Level(int level) {
+        this(level, new String[]{});
+    }
+
+    public Level(int level, String[] developers) {
+        this.level = level;
+        this.developers = developers;
+    }
+
+    public abstract void startLevel();
+    public abstract boolean drawLevel(Canvas canvas);
+}
